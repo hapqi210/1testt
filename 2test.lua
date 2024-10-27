cae6b0cd6b46f02c025541688e6db43095366375
hapqi210 <157613652+hapqi210@users.noreply.github.com>
2024-10-27 18:35:26 +0700

    Add files via upload

--- 2test.lua
+++ 2test.lua
@@ -1,10 +1,17 @@
+function getNil(name,class) for _,v in pairs(getnilinstances())do if v.ClassName==class and v.Name==name then return v;end end end
+
 local args = {
     [1] = "hit",
     [2] = {
-        [1] = workspace.Enemies:FindFirstChild("Cookie Crafter").HumanoidRootPart
+        [1] = getNil("Cookie Crafter", "Model").HumanoidRootPart,
+        [2] = getNil("Cookie Crafter", "Model").HumanoidRootPart,
+        [3] = getNil("Cookie Crafter", "Model").HumanoidRootPart,
+        [4] = getNil("Cookie Crafter", "Model").HumanoidRootPart,
+        [5] = getNil("Cookie Crafter", "Model").HumanoidRootPart,
+        [6] = getNil("Cookie Crafter", "Model").HumanoidRootPart,
+        [7] = getNil("Cookie Crafter", "Model").HumanoidRootPart
     },
-    [3] = 1,
+    [3] = 3,
     [4] = ""
 }
-
 game:GetService("ReplicatedStorage").RigControllerEvent:FireServer(unpack(args))
